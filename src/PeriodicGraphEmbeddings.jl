3da603ca8df11eccb717141e628e48b29057853c
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2026-02-13 11:27:21 +0100

    Update docs setup

--- src/PeriodicGraphEmbeddings.jl
+++ src/PeriodicGraphEmbeddings.jl
@@ -1,11 +1,3 @@
-"""
-PeriodicGraphEmbeddings
-
-A module for manipulating embeddings of periodic graphs in euclidean space.
-
-Symmetry properties are provided on graphs up to dimension 3 through
-[spglib](https://spglib.github.io/spglib/).
-"""
 module PeriodicGraphEmbeddings
 
 using PeriodicGraphs, StaticArrays, Graphs
